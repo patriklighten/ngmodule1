6bce4dab83077833ffcd3e1d8b9e61065ea9b516
Steven Pan <patrik.lighten@gmail.com>
2017-04-11 11:12:42 -0400

    revise code

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.menu=""; 
   $scope.message="";
-
+  $scope.messageStyle={color:'red'};
+  $scope.boxStyle={border: '1px solid red'};
   $scope.sayMessage = function () {
     var menus= $scope.menu; 
     var l= menus.split(',');
@@ -18,9 +19,25 @@ function LunchCheckController($scope) {
     {
       if(l[i].trim().length>0) count++;
     }
-    if(count==0) $scope.message="Please enter data first";
-    else if(count<=3) $scope.message="Enjoy!"; 
-    else $scope.message="Too much!";
+    if(count==0)
+    { 
+      $scope.message="Please enter data first";
+      $scope.messageStyle={color:'red'};
+      $scope.boxStyle={border: '1px solid red'}
+
+    }
+    else if(count<=3) 
+      {
+        $scope.message="Enjoy!"; 
+        $scope.messageStyle={color:'green'};
+        $scope.boxStyle={border: '1px solid green'}
+      }
+    else 
+      {
+        $scope.message="Too much!";
+        $scope.messageStyle={color:'green'};
+        $scope.boxStyle={border: '1px solid green'}
+      }
   };
 
 
